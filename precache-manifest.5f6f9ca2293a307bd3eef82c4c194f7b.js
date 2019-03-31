06843c3eacd3696cb1344e06362000aca83d5b73
Karl Skagersten <karl.skagersten@gmail.com>
2019-03-31 23:58:53 +0200

    Updates

---
+++ precache-manifest.5f6f9ca2293a307bd3eef82c4c194f7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9539b77635bff622dd3d",
+    "url": "/mastermind/static/css/main.3aae2c6a.chunk.css"
+  },
+  {
+    "revision": "9539b77635bff622dd3d",
+    "url": "/mastermind/static/js/main.9d6e0b01.chunk.js"
+  },
+  {
+    "revision": "5ef1bc21d77223317b3d",
+    "url": "/mastermind/static/js/runtime~main.004a5c91.js"
+  },
+  {
+    "revision": "246f946294448963bb77",
+    "url": "/mastermind/static/js/2.10af5e64.chunk.js"
+  },
+  {
+    "revision": "908943291cc4055050cf47fcd0145023",
+    "url": "/mastermind/index.html"
+  }
+];
